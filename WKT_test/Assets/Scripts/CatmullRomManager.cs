971ca4a5dd46f72564ab938602aee8624ffccec7
tony123966 <tony123966@yahoo.com.tw>
2016-01-23 21:23:03 +0800

    20160123

---
+++ WKT_test/Assets/Scripts/CatmullRomManager.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CatmullRomManager : MonoBehaviour {
+
+	public List<Transform> controlPointList = new List<Transform>();
+	public List<Vector3> totalPointList = new List<Vector3>();
+	public GameObject controlPoint;
+	public bool isLoop = false;
+	public float lineWidth=1.0f;
+	public int numberOfPoints = 100;
+
+	private LineRenderer lineRenderer;
+
+	void Start() 
+	{
+		lineRenderer = GetComponent<LineRenderer>();
+		if (!lineRenderer)
+		{
+			gameObject.AddComponent<LineRenderer>();
+			lineRenderer = GetComponent<LineRenderer>();
+		}
+		lineRenderer.useWorldSpace = true;
+		lineRenderer.SetColors(Color.white, Color.white);
+		lineRenderer.SetWidth(lineWidth, lineWidth);
+
+
+	}
+	void FixedUpdate() 
+	{
+		if (controlPointList.Count<4)return;
+		if (isLoop) lineRenderer.SetVertexCount(numberOfPoints * (controlPointList.Count));
+		else lineRenderer.SetVertexCount(numberOfPoints * (controlPointList.Count-3));
+		DisplayCatmullromSpline();
+		RenderCatmullromSpline();
+	}
+
+	Vector3 ReturnCatmullRomPos(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
+	{
+		Vector3 pos = 0.5f * ((2f * p1) + (-p0 + p2) * t + (2f * p0 - 5f * p1 + 4f * p2 - p3) * t * t + (-p0 + 3f * p1 - 3f * p2 + p3) * t * t * t);
+		return pos;
+	}
+	void DisplayCatmullromSpline()
+	{
+		totalPointList.Clear();
+		for (int index = 0; index < controlPointList.Count; index++)
+		{
+			if(!isLoop){
+				if ((index == 0 || index == controlPointList.Count - 2 || index == controlPointList.Count - 1))
+				{
+					continue;
+				}
+			}
+			Vector3 p0 = controlPointList[(index - 1 + controlPointList.Count) % controlPointList.Count].transform.position;
+			Vector3 p1 = controlPointList[(index + controlPointList.Count) % controlPointList.Count].transform.position;
+			Vector3 p2 = controlPointList[(index + 1 + controlPointList.Count) % controlPointList.Count].transform.position;
+			Vector3 p3 = controlPointList[(index + 2 + controlPointList.Count) % controlPointList.Count].transform.position;
+			
+			float segmentation = 1/ (float)numberOfPoints;
+			float t=0;
+			for(int i=0;i<numberOfPoints;i++){
+				Vector3 newPos=ReturnCatmullRomPos(t,p0, p1,p2, p3);
+				totalPointList.Add(newPos);
+				t += segmentation;
+			}
+		}
+	}
+	void RenderCatmullromSpline() 
+	{ 
+		for(int i=0;i<totalPointList.Count;i++){
+			lineRenderer.SetPosition(i, totalPointList[i]);
+		}
+	}
+
+	public void AddControlPoint(Vector3 point)
+	{
+		GameObject clone;
+		clone = Instantiate(controlPoint, point, controlPoint.transform.rotation)as GameObject;
+		clone.transform.parent=gameObject.transform;
+		controlPointList.Add(clone.transform);
+	}
+	public void MoveControlPoint(GameObject obj,Vector3 point)
+	{
+		obj.transform.position=point;
+	}
+	public void removeControlPoint(GameObject obj)
+	{
+		SortControlPoint(obj);
+		Destroy(obj);
+	}
+	void SortControlPoint(GameObject obj){
+		for (int i = 0; i < controlPointList.Count; i++)
+		{
+			if (controlPointList[i].gameObject == obj)
+			{ 
+				for(int j=i;j<controlPointList.Count;j++)
+				{
+					controlPointList[j]=controlPointList[j+1];
+				}
+				controlPointList.Capacity--;
+				return;
+			}
+		}
+	}
+}
---
+++ WKT_test/Assets/Scripts/ColliderDetection.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColliderDetection : MonoBehaviour {
+	public GameObject mainCamera;
+	public GameObject collisionPlane;
+	public CatmullRomManager catmullRomManager;
+	
+	public GameObject chooseObj=null;
+	public Vector3 clickPos = Vector3.zero;
+	void Update() 
+	{
+		if (chooseObj) 
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit))
+			{
+				if (hit.collider.gameObject.name == collisionPlane.gameObject.name)
+				{
+					catmullRomManager.MoveControlPoint(chooseObj, hit.point);
+				}
+			}
+			if (Input.GetMouseButtonUp(0)) 
+			{ 
+				chooseObj=null;
+			}
+		}
+		else {
+			if (Input.GetMouseButtonDown(0))
+			{
+				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+				RaycastHit hit;
+				if (Physics.Raycast(ray, out hit))
+				{
+					if (hit.collider.gameObject.name == collisionPlane.gameObject.name)
+					{
+						catmullRomManager.AddControlPoint(hit.point);
+					}
+					else if (hit.collider.gameObject.tag == "controlPoint")
+					{
+						chooseObj = hit.collider.gameObject;
+						clickPos = hit.point;
+					}
+				}
+			}
+		}
+			if (Input.GetMouseButtonDown(1))
+			{
+				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+				RaycastHit hit;
+				if (Physics.Raycast(ray, out hit))
+				{
+					if (hit.collider.gameObject.tag == "controlPoint")
+					{
+						catmullRomManager.removeControlPoint(hit.collider.gameObject);
+						chooseObj=null;
+						clickPos = hit.point
+					}
+				}
+			}
+	}
+}
